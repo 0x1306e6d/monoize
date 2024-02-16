6a7c6c47ced852e6d1729a647181baf09d0b5601
Gihwan Kim <ghkim3221@gmail.com>
2024-02-16 17:18:15 +0900

    Get source and target repositories with basic auth parameters

--- cmd/root.go
+++ cmd/root.go
@@ -6,38 +6,58 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/spf13/cobra"
 )
 
+var (
+	username string
+	password string
+)
+
 var rootCmd = &cobra.Command{
-	Use:   "monoize",
+	Use:   "monoize [source repository] [target repository]",
 	Short: "monoize makes your git repositories monorepo",
+	Args:  cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		repo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-			URL: "https://github.com/go-git/go-billy",
-		})
-		if err != nil {
-			panic(err)
+		sources := args[:len(args)-1]
+		// target := args[len(args)-1]
+
+		for _, source := range sources {
+			repo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+				Auth: &http.BasicAuth{
+					Username: username, Password: password,
+				},
+				URL: source,
+			})
+			if err != nil {
+				panic(err)
+			}
+
+			ref, err := repo.Head()
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(ref)
+
+			cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+			if err != nil {
+				panic(err)
+			}
+
+			cIter.ForEach(func(c *object.Commit) error {
+				fmt.Println(c)
+				return nil
+			})
 		}
-
-		ref, err := repo.Head()
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(ref)
-
-		cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
-		if err != nil {
-			panic(err)
-		}
-
-		cIter.ForEach(func(c *object.Commit) error {
-			fmt.Println(c)
-			return nil
-		})
 	},
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&username, "username", "u", "", "username for auth")
+	rootCmd.Flags().StringVarP(&password, "password", "p", "", "password for auth")
 }
 
 func Execute() {
