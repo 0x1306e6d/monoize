f324e0f7bc8ff561c5fe52d63be9276f44e1a062
Gihwan Kim <ghkim3221@gmail.com>
2024-02-16 16:27:42 +0900

    Add go-git

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/memory"
+)
 
 func main() {
-	fmt.Printf("Hello, world!")
+	repo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: "https://github.com/go-git/go-billy",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	ref, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(ref)
+
+	cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		panic(err)
+	}
+
+	cIter.ForEach(func(c *object.Commit) error {
+		fmt.Println(c)
+		return nil
+	})
 }
